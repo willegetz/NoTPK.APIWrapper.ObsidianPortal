2cd9081cb9e8d5884c1467628b4567df4cf87615
William Getz <willegetz@gmail.com>
2014-05-10 21:08:00 -0700

    Added ability to get the authorization header for a request.

---
+++ NoTPK.APIWrapper.ObsidianPortal/NoTPK.APIWrapper.ObsidianPortal/APICalls.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace NoTPK.APIWrapper.ObsidianPortal
+{
+	public class ApiCalls
+	{
+		private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+		public static string GetAuthorizationHeader(string appId, string appSecret, string accessToken, string accessTokenSecret, string location, HttpMethod webMethod)
+		{
+			string nonce = Guid.NewGuid().ToString("N");
+
+			var authorizationParts = new SortedDictionary<string, string>
+			{
+				{ "oauth_consumer_key", appId },
+				{ "oauth_nonce", nonce },
+				{ "oauth_signature_method", "HMAC-SHA1" },
+				{ "oauth_token", accessToken },
+				{ "oauth_timestamp", GenerateTimeStamp() },
+				{ "oauth_version", "1.0" },
+			};
+
+			var parameterBuilder = new StringBuilder();
+			foreach (var authorizationKey in authorizationParts)
+			{
+				parameterBuilder.AppendFormat("{0}={1}&", Uri.EscapeDataString(authorizationKey.Key), Uri.EscapeDataString(authorizationKey.Value));
+			}
+			parameterBuilder.Length--;
+			string parameterString = parameterBuilder.ToString();
+
+			var canonicalizedRequestBuilder = new StringBuilder();
+			canonicalizedRequestBuilder.Append(webMethod.Method);
+			canonicalizedRequestBuilder.Append("&");
+			canonicalizedRequestBuilder.Append(Uri.EscapeDataString(location));
+			canonicalizedRequestBuilder.Append("&");
+			canonicalizedRequestBuilder.Append(Uri.EscapeDataString(parameterString));
+
+			string signature = ComputeSignature(appSecret, accessTokenSecret, canonicalizedRequestBuilder.ToString());
+			authorizationParts.Add("oauth_signature", signature);
+			authorizationParts.Remove("oauth_verifier");
+
+			var authorizationHeaderBuilder = new StringBuilder();
+			authorizationHeaderBuilder.Append("OAuth ");
+			foreach (var authorizationPart in authorizationParts)
+			{
+				authorizationHeaderBuilder.AppendFormat(
+					"{0}=\"{1}\", ", authorizationPart.Key, Uri.EscapeDataString(authorizationPart.Value));
+			}
+			authorizationHeaderBuilder.Length = authorizationHeaderBuilder.Length - 2;
+			return authorizationHeaderBuilder.ToString();
+		}
+
+		private static string ComputeSignature(string appSecret, string tokenSecret, string signatureData)
+		{
+			using (var algorithm = new HMACSHA1())
+			{
+				var escapedConsumerSecret = Uri.EscapeDataString(appSecret);
+				var escapedTokenSecret = string.IsNullOrEmpty(tokenSecret) ? string.Empty : Uri.EscapeDataString(tokenSecret);
+
+				algorithm.Key = Encoding.ASCII.GetBytes(string.Format(CultureInfo.InvariantCulture, "{0}&{1}", escapedConsumerSecret, escapedTokenSecret));
+				byte[] hash = algorithm.ComputeHash(Encoding.ASCII.GetBytes(signatureData));
+				return Convert.ToBase64String(hash);
+			}
+		}
+
+		private static string GenerateTimeStamp()
+		{
+			TimeSpan secondsSinceUnixEpocStart = DateTime.UtcNow - Epoch;
+			return Convert.ToInt64(secondsSinceUnixEpocStart.TotalSeconds).ToString(CultureInfo.InvariantCulture);
+		}
+
+	}
+}
