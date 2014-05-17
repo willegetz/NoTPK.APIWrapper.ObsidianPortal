299cf7155fda16e552427f14d1607dcaf4b767dd
William Getz <willegetz@gmail.com>
2014-05-17 08:29:20 -0700

    loading approved data from xml file and comparing.

--- NoTPK.APIWrapper.ObsidianPortal/APIWrapper.ObsidianPortal.Tests/API_Users_Tests.cs
+++ NoTPK.APIWrapper.ObsidianPortal/APIWrapper.ObsidianPortal.Tests/API_Users_Tests.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
-using APIWrapper.ObsidianPortal.Tests.Support;
+using System.Xml.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NoTPK.APIWrapper.ObsidianPortal;
 
@@ -9,16 +10,40 @@ namespace APIWrapper.ObsidianPortal.Tests
 	[TestClass]
 	public class API_Users_Tests
 	{
-		private const string AppId = "";
-		private const string AppSecret = "";
-		private const string Token = "";
-		private const string TokenSecret = "";
+		private static string _appId = "";
+		private static string _appSecret = "";
+		private static string _token = "";
+		private static string _tokenSecret = "";
+
+		private static XElement _approvedResults;
+
+		[AssemblyInitialize]
+		public static void LoadConstants(TestContext testContext)
+		{
+			var configPath = Path.GetFullPath(@"..\..\..\..\..\..\Configs\NoTPK.APIWrapper.ObsidianPortal.Tests.Config.xml");
+			var tokenDoc = XDocument.Load(configPath);
+			var tokens = (from t in tokenDoc.Descendants("TestTokens") select t).FirstOrDefault();
+
+			if (tokens != null)
+			{
+				_appId = (string) tokens.Element("AppId");
+				_appSecret = (string)tokens.Element("AppSecret");
+				_token = (string)tokens.Element("AccessToken");
+				_tokenSecret = (string)tokens.Element("AccessTokenSecret");
+			}
+
+			var approvedPath = Path.GetFullPath(@"..\..\..\..\..\..\Configs\NoTPK.APIWrapper.ObsidianPortal.Tests.Approved.xml");
+			var approvedDOc = XDocument.Load(approvedPath);
 			
+			_approvedResults = (from a in approvedDOc.Descendants("ApprovedValues") select a).FirstOrDefault();
+		}
+
 		[TestMethod]
 		public async Task Test_Users_Show__LoggedInUser()
 		{
-			var result = await ApiCalls.ShowMe(AppId, AppSecret, Token, TokenSecret);
-			Assert.AreEqual("", result);
+			var approved = (string) _approvedResults.Element("Show_LoggedInUser");
+			var result = await ApiCalls.ShowMe(_appId, _appSecret, _token, _tokenSecret);
+			Assert.AreEqual(approved, result);
 		}
 	}
 }
