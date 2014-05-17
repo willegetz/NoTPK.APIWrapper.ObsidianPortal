33d39ce4545cd51400fcdcbe2cdef7b3ea13be9d
William Getz <willegetz@gmail.com>
2014-05-16 18:41:21 -0700

    Begin of unit testing. Have failing test.

---
+++ NoTPK.APIWrapper.ObsidianPortal/APIWrapper.ObsidianPortal.Tests/API_Users_Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using APIWrapper.ObsidianPortal.Tests.Support;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NoTPK.APIWrapper.ObsidianPortal;
+
+namespace APIWrapper.ObsidianPortal.Tests
+{
+	[TestClass]
+	public class API_Users_Tests
+	{
+		private const string AppId = "";
+		private const string AppSecret = "";
+		private const string Token = "";
+		private const string TokenSecret = "";
+			
+		[TestMethod]
+		public async Task Test_Users_Show__LoggedInUser()
+		{
+			var result = await ApiCalls.ShowMe(AppId, AppSecret, Token, TokenSecret);
+			Assert.AreEqual("", result);
+		}
+	}
+}
---
+++ NoTPK.APIWrapper.ObsidianPortal/APIWrapper.ObsidianPortal.Tests/Support/Helpers.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Web.Script.Serialization;
+
+namespace APIWrapper.ObsidianPortal.Tests.Support
+{
+	public class Helpers
+	{
+		public static Dictionary<string, object> ParseJson(string jsonText)
+		{
+			var serializer = new JavaScriptSerializer();
+			return serializer.Deserialize<Dictionary<string, object>>(jsonText);
+		} 
+	}
+}
