3901619436a4de03428ca68e59887c0960ee39d1
William Getz <willegetz@gmail.com>
2014-05-18 21:53:47 -0700

    Can retrieve all DST's with Index and specific DST with id.

---
+++ NoTPK.APIWrapper.ObsidianPortal/APIWrapper.ObsidianPortal.Tests/API_DynamicSheetTemplates_Tests.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NoTPK.APIWrapper.ObsidianPortal;
+
+namespace APIWrapper.ObsidianPortal.Tests
+{
+	[TestClass]
+	public class API_DynamicSheetTemplates_Tests
+	{
+		private static string _appId = "";
+		private static string _appSecret = "";
+		private static string _token = "";
+		private static string _tokenSecret = "";
+
+		private static XElement _approvedResults;
+		private static XElement _testVariables;
+
+		[ClassInitialize]
+		public static void LoadConstants(TestContext testContext)
+		{
+			var configPath = Path.GetFullPath(@"..\..\..\..\..\..\Configs\NoTPK.APIWrapper.ObsidianPortal.Tests.Config.xml");
+			var configDoc = XDocument.Load(configPath);
+			var tokens = (from t in configDoc.Descendants("TestTokens") select t).FirstOrDefault();
+
+			if (tokens != null)
+			{
+				_appId = (string)tokens.Element("AppId");
+				_appSecret = (string)tokens.Element("AppSecret");
+				_token = (string)tokens.Element("AccessToken");
+				_tokenSecret = (string)tokens.Element("AccessTokenSecret");
+			}
+
+			var approvedPath = Path.GetFullPath(@"..\..\..\..\..\..\Configs\NoTPK.APIWrapper.ObsidianPortal.Tests.Approved.xml");
+			var approvedDOc = XDocument.Load(approvedPath);
+
+			_approvedResults = (from a in approvedDOc.Descendants("ApprovedValues").Descendants("DSTs") select a).FirstOrDefault();
+			_testVariables = (from v in configDoc.Descendants("TestVariables") select v).FirstOrDefault();
+		}
+
+		[TestMethod]
+		public async Task Test_DynamicSheetTemplates_Index()
+		{
+			var approved = (string) _approvedResults.Element("Index_DSTs");
+
+			var result = await API_DynamicSheetTemplates.Index(_appId, _appSecret, _token, _tokenSecret);
+			Assert.AreEqual(approved, result);
+		}
+
+		[TestMethod]
+		public async Task Test_DynamicSheetTemplates_Show__ById()
+		{
+			var approved = (string) _approvedResults.Element("Show_DST");
+			var dstId = (string) _testVariables.Element("DST_Id");
+
+			var restul = await API_DynamicSheetTemplates.Show(_appId, _appSecret, _token, _tokenSecret, dstId);
+			Assert.AreEqual(approved, restul);
+		}
+	}
+}
---
+++ NoTPK.APIWrapper.ObsidianPortal/NoTPK.APIWrapper.ObsidianPortal/API_DynamicSheetTemplates.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using NoTPK.APIWrapper.ObsidianPortal.Helpers;
+
+namespace NoTPK.APIWrapper.ObsidianPortal
+{
+	public class API_DynamicSheetTemplates
+	{
+		public static async Task<string> Index(string appId, string appSecret, string token, string tokenSecret)
+		{
+			const string indexUrl = "http://api.obsidianportal.com/v1/dynamic_sheet_templates.json";
+
+			var requestMessage = RequestHelpers.BuildRequest(appId, appSecret, token, tokenSecret, indexUrl, HttpMethod.Get);
+			return await RequestHelpers.RetrieveResponseContent(requestMessage);
+		}
+
+		public static async Task<string> Show(string appId, string appSecret, string token, string tokenSecret, string dstId)
+		{
+			var showUrl = string.Format(@"http://api.obsidianportal.com/v1/dynamic_sheet_templates/{0}.json", dstId);
+
+			var requestMessage = RequestHelpers.BuildRequest(appId, appSecret, token, tokenSecret, showUrl, HttpMethod.Get);
+			return await RequestHelpers.RetrieveResponseContent(requestMessage);
+		}
+	}
+}
