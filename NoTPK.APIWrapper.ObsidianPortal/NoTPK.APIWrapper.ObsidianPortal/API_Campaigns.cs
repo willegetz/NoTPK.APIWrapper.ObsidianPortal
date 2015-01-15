0a80bc355c1da6ce1399a311482a26e4175491ee
William Getz <willegetz@gmail.com>
2015-01-15 00:16:27 -0800

    Converting return object to json

--- NoTPK.APIWrapper.ObsidianPortal/NoTPK.APIWrapper.ObsidianPortal/API_Campaigns.cs
+++ NoTPK.APIWrapper.ObsidianPortal/NoTPK.APIWrapper.ObsidianPortal/API_Campaigns.cs
@@ -3,27 +3,28 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using NoTPK.APIWrapper.ObsidianPortal.Helpers;
+using Newtonsoft.Json.Linq;
 
 namespace NoTPK.APIWrapper.ObsidianPortal
 {
 	public class API_Campaigns
 	{
-		public static async Task<string> ShowById(string appId, string appSecret, string token, string tokenSecret, string campaignId)
+		public static async Task<JObject> ShowById(string appId, string appSecret, string token, string tokenSecret, string campaignId)
 		{
 			string showUrl = string.Format(@"http://api.obsidianportal.com/v1/campaigns/{0}.json", campaignId);
 
 			var requestMessage = RequestHelpers.BuildRequest(appId, appSecret, token, tokenSecret, showUrl, HttpMethod.Get);
-			return await RequestHelpers.RetrieveResponseContent(requestMessage);
+			return await RequestHelpers.RetrieveResponseContentJobj(requestMessage);
 		}
 
-		public static async Task<string> ShowBySlug(string appId, string appSecret, string accessToken, string accessTokenSecret, string slug)
+		public static async Task<JObject> ShowBySlug(string appId, string appSecret, string accessToken, string accessTokenSecret, string slug)
 		{
 			string showUrl = string.Format(@"http://api.obsidianportal.com/v1/campaigns/{0}.json", slug);
 			
 			var optionalParams = new Dictionary<string, string>();
 			optionalParams.Add("use_slug", "true");
 			var requestMessage = RequestHelpers.BuildRequest(appId, appSecret, accessToken, accessTokenSecret, showUrl, HttpMethod.Get,"?use_slug=true", optionalParams);
-			return await RequestHelpers.RetrieveResponseContent(requestMessage);
+			return await RequestHelpers.RetrieveResponseContentJobj(requestMessage);
 		}
 	}
 }
