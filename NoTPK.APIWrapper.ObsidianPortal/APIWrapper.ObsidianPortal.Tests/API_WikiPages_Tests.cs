996cf4625b2af108af9ebead36b30bd74d48f271
William Getz <willegetz@gmail.com>
2015-01-15 06:10:24 -0800

    Migrate towards JSON. Refactored async tests.

--- NoTPK.APIWrapper.ObsidianPortal/APIWrapper.ObsidianPortal.Tests/API_WikiPages_Tests.cs
+++ NoTPK.APIWrapper.ObsidianPortal/APIWrapper.ObsidianPortal.Tests/API_WikiPages_Tests.cs
@@ -5,10 +5,13 @@ using System.Xml.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NoTPK.APIWrapper.ObsidianPortal;
 using APIWrapper.ObsidianPortal.Tests.Support;
+using ApprovalTests;
+using ApprovalTests.Reporters;
 
 namespace APIWrapper.ObsidianPortal.Tests
 {
 	[TestClass]
+	[UseReporter(typeof(DiffReporter))]
 	public class API_WikiPages_Tests
 	{
 		private static string _appId = "";
@@ -37,74 +40,68 @@ namespace APIWrapper.ObsidianPortal.Tests
 		}
 
 		[TestMethod]
-		public async Task Test_WikiPages_Index__ByCampaignId()
+		public void Test_WikiPages_Index__ByCampaignId()
 		{
-			var approved = Helpers.GetApprovedResults("Index_WikiPages");
 			var campaignId = (string) _testVariables.Element("CampaignId");
 			
-			var result = await API_WikiPages.IndexByCampaignId(_appId, _appSecret, _token, _tokenSecret, campaignId);
-			Assert.AreEqual(approved, result);
+			var result = API_WikiPages.IndexByCampaignId(_appId, _appSecret, _token, _tokenSecret, campaignId).Result;
+			Approvals.Verify(result);
 		}
 
 		[TestMethod]
-		public async Task Test_WikiPages_Show__ByWikiPageId()
+		public void Test_WikiPages_Show__ByWikiPageId()
 		{
-			var approved = Helpers.GetApprovedResults("Show_WikiPageById");
 			var campaignId = (string) _testVariables.Element("CampaignId");
 			var wikiPageId = (string) _testVariables.Element("WikiPageId");
 
-			var result = await API_WikiPages.ShowById(_appId, _appSecret, _token, _tokenSecret, campaignId, wikiPageId);
-			Assert.AreEqual(approved, result);
+			var result = API_WikiPages.ShowById(_appId, _appSecret, _token, _tokenSecret, campaignId, wikiPageId).Result;
+			Approvals.Verify(result);
 		}
 
 		[TestMethod]
 		/// There are two types of wiki pages: Post and WikiPage. ShowBySlug sometimes works with WikiPages, and not Posts; even then, it may not work.
-		public async Task Test_WikiPages_Show__BySlug()
+		public void Test_WikiPages_Show__BySlug()
 		{
-			var approved = Helpers.GetApprovedResults("Show_WikiPageBySlug");
 			var campaignId = (string) _testVariables.Element("CampaignId");
 			var wikiPageSlug = (string) _testVariables.Element("WikiPageSlug");
 
-			var result = await API_WikiPages.ShowBySlug(_appId, _appSecret, _token, _tokenSecret, campaignId, wikiPageSlug);
-			Assert.AreEqual(approved, result);
+			var result = API_WikiPages.ShowBySlug(_appId, _appSecret, _token, _tokenSecret, campaignId, wikiPageSlug).Result;
+			Approvals.Verify(result);
 		}
 
 		[TestMethod]
 		[Ignore] // Destructive -- Need mocking, perhaps
 
-		public async Task Test_WikiPages_Create__ByCampaignId()
+		public void Test_WikiPages_Create__ByCampaignId()
 		{
-			var approved = Helpers.GetApprovedResults("Create_WikiPage");
 			var campaignId = (string) _testVariables.Element("ModifiableCampaign");
 			var newWikiPage = (string) _testVariables.Element("NewWikiPage");
 
-			var result = await API_WikiPages.Create(_appId, _appSecret, _token, _tokenSecret, campaignId, newWikiPage);
-			Assert.AreEqual(approved, result);
+			var result = API_WikiPages.Create(_appId, _appSecret, _token, _tokenSecret, campaignId, newWikiPage).Result;
+			Approvals.Verify(result);
 		}
 
 		[TestMethod]
 		[Ignore] // Destructive -- Need mocking, perhaps
-		public async Task Test_WikiPages_Update__ByWikiPageId()
+		public void Test_WikiPages_Update__ByWikiPageId()
 		{
-			var approved = Helpers.GetApprovedResults("Update_WikiPage");
 			var campaignId = (string) _testVariables.Element("ModifiableCampaign");
 			var updateWikiPageId = (string) _testVariables.Element("UpdateWikiPageId");
 			var updateWikiPageContent = (string) _testVariables.Element("UpdateWikiContent");
 
-			var result = await API_WikiPages.Update(_appId, _appSecret, _token, _tokenSecret, campaignId, updateWikiPageId, updateWikiPageContent);
-			Assert.AreEqual(approved, result);
+			var result = API_WikiPages.Update(_appId, _appSecret, _token, _tokenSecret, campaignId, updateWikiPageId, updateWikiPageContent).Result;
+			Approvals.Verify(result);
 		}
 
 		[TestMethod]
 		[Ignore] // Destructive -- Need mocking, perhaps
-		public async Task Test_WikiPages_Delete__ByWikiPageId()
+		public void Test_WikiPages_Delete__ByWikiPageId()
 		{
-			var approved = Helpers.GetApprovedResults("Delete_WikiPage");
 			var campaignId = (string) _testVariables.Element("ModifiableCampaign");
 			var deleteWikipageId = (string) _testVariables.Element("DeleteWikiPageId");
 
-			var result = await API_WikiPages.Delete(_appId, _appSecret, _token, _tokenSecret, campaignId, deleteWikipageId);
-			Assert.AreEqual(approved, result);
+			var result = API_WikiPages.Delete(_appId, _appSecret, _token, _tokenSecret, campaignId, deleteWikipageId).Result;
+			Approvals.Verify(result);
 		}
 	}
 }
--- NoTPK.APIWrapper.ObsidianPortal/NoTPK.APIWrapper.ObsidianPortal/API_WikiPages.cs
+++ NoTPK.APIWrapper.ObsidianPortal/NoTPK.APIWrapper.ObsidianPortal/API_WikiPages.cs
@@ -4,61 +4,62 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using NoTPK.APIWrapper.ObsidianPortal.Helpers;
+using Newtonsoft.Json.Linq;
 
 namespace NoTPK.APIWrapper.ObsidianPortal
 {
 	public class API_WikiPages
 	{
-		public static async Task<string> IndexByCampaignId(string appId, string appSecret, string token, string tokenSecret, string campaignId)
+		public static async Task<JArray> IndexByCampaignId(string appId, string appSecret, string token, string tokenSecret, string campaignId)
 		{
 			var indexUrl = string.Format(@"http://api.obsidianportal.com/v1/campaigns/{0}/wikis.json", campaignId);
 
 			var requestMessage = RequestHelpers.BuildRequest(appId, appSecret, token, tokenSecret, indexUrl, HttpMethod.Get);
-			return await RequestHelpers.RetrieveResponseContent(requestMessage);
+			return await RequestHelpers.RetrieveResponseContentJarray(requestMessage);
 		}
 
-		public static async Task<string> ShowById(string appId, string appSecret, string token, string tokenSecret, string campaignId, string wikiPageId)
+		public static async Task<JObject> ShowById(string appId, string appSecret, string token, string tokenSecret, string campaignId, string wikiPageId)
 		{
 			var showUrl = string.Format(@"http://api.obsidianportal.com/v1/campaigns/{0}/wikis/{1}.json", campaignId, wikiPageId);
 
 			var requestMessage = RequestHelpers.BuildRequest(appId, appSecret, token, tokenSecret, showUrl, HttpMethod.Get);
-			return await RequestHelpers.RetrieveResponseContent(requestMessage);
+			return await RequestHelpers.RetrieveResponseContentJobj(requestMessage);
 		}
 
-		public static async Task<string> ShowBySlug(string appId, string appSecret, string token, string tokenSecret, string campaignId, string wikiPageSlug)
+		public static async Task<JObject> ShowBySlug(string appId, string appSecret, string token, string tokenSecret, string campaignId, string wikiPageSlug)
 		{
 			var showUrl = string.Format(@"http://api.obsidianportal.com/v1/campaigns/{0}/wikis/{1}.json", campaignId, wikiPageSlug);
 			var optionalParams = new Dictionary<string, string>();
 			optionalParams.Add("use_slug", "true");
 
 			var requestMessage = RequestHelpers.BuildRequest(appId, appSecret, token, tokenSecret, showUrl, HttpMethod.Get, "?use_slug=true", optionalParams);
-			return await RequestHelpers.RetrieveResponseContent(requestMessage);
+			return await RequestHelpers.RetrieveResponseContentJobj(requestMessage);
 		}
 
-		public static async Task<string> Create(string appId, string appSecret, string token, string tokenSecret, string campaignId, string wikiPageJson)
+		public static async Task<JObject> Create(string appId, string appSecret, string token, string tokenSecret, string campaignId, string wikiPageJson)
 		{
 			var createUrl = string.Format(@"http://api.obsidianportal.com/v1/campaigns/{0}/wikis.json", campaignId);
 
 			var requestMessage = RequestHelpers.BuildRequest(appId, appSecret, token, tokenSecret, createUrl, HttpMethod.Post);
 			requestMessage.Content = new StringContent(wikiPageJson, Encoding.UTF8, "application/json");
-			return await RequestHelpers.RetrieveResponseContent(requestMessage);
+			return await RequestHelpers.RetrieveResponseContentJobj(requestMessage);
 		}
 
-		public static async Task<string> Update(string appId, string appSecret, string token, string tokenSecret, string campaignId, string wikiPageId, string wikiPageJson)
+		public static async Task<JObject> Update(string appId, string appSecret, string token, string tokenSecret, string campaignId, string wikiPageId, string wikiPageJson)
 		{
 			var updateUrl = string.Format(@"http://api.obsidianportal.com/v1/campaigns/{0}/wikis/{1}.json", campaignId, wikiPageId);
 
 			var requestMessage = RequestHelpers.BuildRequest(appId, appSecret, token, tokenSecret, updateUrl, HttpMethod.Put);
 			requestMessage.Content = new StringContent(wikiPageJson, Encoding.UTF8, "application/json");
-			return await RequestHelpers.RetrieveResponseContent(requestMessage);
+			return await RequestHelpers.RetrieveResponseContentJobj(requestMessage);
 		}
 
-		public static async Task<string> Delete(string appId, string appSecret, string token, string tokenSecret, string campaignId, string wikiPageId)
+		public static async Task<JObject> Delete(string appId, string appSecret, string token, string tokenSecret, string campaignId, string wikiPageId)
 		{
 			var deleteUrl = string.Format(@"http://api.obsidianportal.com/v1/campaigns/{0}/wikis/{1}.json", campaignId, wikiPageId);
 
 			var requestMessage = RequestHelpers.BuildRequest(appId, appSecret, token, tokenSecret, deleteUrl, HttpMethod.Delete);
-			return await RequestHelpers.RetrieveResponseContent(requestMessage);
+			return await RequestHelpers.RetrieveResponseContentJobj(requestMessage);
 		}
 	}
 }
