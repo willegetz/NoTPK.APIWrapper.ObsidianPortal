c50983bd2ad34e886f4d6dbffdb35bda3607ee52
William Getz <willegetz@gmail.com>
2015-10-04 21:37:39 -0700

    Begin testing request helper.

---
+++ NoTPK.APIWrapper.ObsidianPortal/APIWrapper.ObsidianPortal.Tests/RequestHelper_Tests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NoTPK.APIWrapper.ObsidianPortal.Helpers;
+using ApprovalTests;
+using System.Net.Http;
+
+namespace APIWrapper.ObsidianPortal.Tests
+{
+    [TestClass]
+    public class RequestHelper_Tests
+    {
+        [TestMethod]
+        public void TestGetAuthorizationHeader()
+        {
+            var appId = "appId";
+            var appSecret = "appSecret";
+            var accessToken = "accessToken";
+            var accessTokenSecret = "accessTokenSecret";
+            var location = "location";
+            var nonce = "nonce";
+            var webMethod = HttpMethod.Get;
+
+            var result = RequestHelpers.GetAuthorizationHeader(appId, appSecret, accessToken, accessTokenSecret, location, webMethod, nonce);
+            Approvals.Verify(result);
+        }
+
+        [TestMethod]
+        public void TestGetAuthorizationParts()
+        {
+            var appId = "appId";
+            var accessToken = "accessToken";
+            var nonce = "nonce";
+            var oauthTimestamp = "1444009267";
+            var optionalParams = new Dictionary<string, string>()
+            {
+                {"optionalKey1", "optionalValue1" },
+                {"optionalKey2", "optionalValue2" },
+                {"optionalKey3", "optionalValue3" },
+            };
+
+            var result = RequestHelpers.GetAuthorizationParts(appId, accessToken, nonce, oauthTimestamp, optionalParams);
+            Approvals.VerifyAll(result);
+        }
+
+        [TestMethod]
+        public void TestGetAuthorizationParts_NoOptionalParams()
+        {
+            var appId = "appId";
+            var accessToken = "accessToken";
+            var nonce = "nonce";
+            var oauthTimestamp = "1444009267";
+
+            var result = RequestHelpers.GetAuthorizationParts(appId, accessToken, nonce, oauthTimestamp, null);
+            Approvals.VerifyAll(result);
+        }
+    }
+}
--- NoTPK.APIWrapper.ObsidianPortal/NoTPK.APIWrapper.ObsidianPortal/Helpers/RequestHelpers.cs
+++ NoTPK.APIWrapper.ObsidianPortal/NoTPK.APIWrapper.ObsidianPortal/Helpers/RequestHelpers.cs
@@ -65,7 +65,8 @@ namespace NoTPK.APIWrapper.ObsidianPortal.Helpers
 
 		internal static HttpRequestMessage BuildRequest(string appId, string appSecret, string accessToken, string accessTokenSecret, string location, HttpMethod webMethod, string queryParams = "", Dictionary<string, string> optionalParams = null)
 		{
-			var authorizationHeader = GetAuthorizationHeader(appId, appSecret, accessToken, accessTokenSecret, location, webMethod, optionalParams);
+            string nonce = Guid.NewGuid().ToString("N");
+            var authorizationHeader = GetAuthorizationHeader(appId, appSecret, accessToken, accessTokenSecret, location, webMethod, nonce, optionalParams);
 
 			var fullUri = location + queryParams;
 			var request = new HttpRequestMessage(webMethod, fullUri);
@@ -73,65 +74,71 @@ namespace NoTPK.APIWrapper.ObsidianPortal.Helpers
 			return request;
 		}
 
-		private static string GetAuthorizationHeader(string appId, string appSecret, string accessToken, string accessTokenSecret, string location, HttpMethod webMethod, Dictionary<string, string> optionalParams = null)
-		{
-			string nonce = Guid.NewGuid().ToString("N");
+		public static string GetAuthorizationHeader(string appId, string appSecret, string accessToken, string accessTokenSecret, string location, HttpMethod webMethod, string nonce, Dictionary<string, string> optionalParams = null)
+        {
+            var oauthTimestamp = GenerateTimeStamp();
+            SortedDictionary<string, string> authorizationParts = GetAuthorizationParts(appId, accessToken, nonce, oauthTimestamp, optionalParams);
 
-			var authorizationParts = new SortedDictionary<string, string>()
-			{
-				{"oauth_consumer_key", appId},
-				{"oauth_nonce", nonce},
-				{"oauth_signature_method", "HMAC-SHA1"},
-				{"oauth_token", accessToken},
-				{"oauth_timestamp", GenerateTimeStamp()},
-				{"oauth_version", "1.0"},
-			};
+            var parameterBuilder = new StringBuilder();
+            foreach (var authorizationKey in authorizationParts)
+            {
+                parameterBuilder.AppendFormat("{0}={1}&", Uri.EscapeDataString(authorizationKey.Key), Uri.EscapeDataString(authorizationKey.Value));
+            }
+            parameterBuilder.Length--;
+            string parameterString = parameterBuilder.ToString();
 
-			if (optionalParams != null)
-			{
-				foreach (var param in optionalParams)
-				{
-					authorizationParts.Add(param.Key, param.Value);
-				}
-			}
+            var canonicalizedRequestBuilder = new StringBuilder();
+            canonicalizedRequestBuilder.Append(webMethod.Method);
+            canonicalizedRequestBuilder.Append("&");
+            canonicalizedRequestBuilder.Append(Uri.EscapeDataString(location));
+            canonicalizedRequestBuilder.Append("&");
+            canonicalizedRequestBuilder.Append(Uri.EscapeDataString(parameterString));
 
-			var parameterBuilder = new StringBuilder();
-			foreach (var authorizationKey in authorizationParts)
-			{
-				parameterBuilder.AppendFormat("{0}={1}&", Uri.EscapeDataString(authorizationKey.Key), Uri.EscapeDataString(authorizationKey.Value));
-			}
-			parameterBuilder.Length--;
-			string parameterString = parameterBuilder.ToString();
+            string signature = ComputeSignature(appSecret, accessTokenSecret, canonicalizedRequestBuilder.ToString());
+            authorizationParts.Add("oauth_signature", signature);
+            authorizationParts.Remove("oauth_verifier");
 
-			var canonicalizedRequestBuilder = new StringBuilder();
-			canonicalizedRequestBuilder.Append(webMethod.Method);
-			canonicalizedRequestBuilder.Append("&");
-			canonicalizedRequestBuilder.Append(Uri.EscapeDataString(location));
-			canonicalizedRequestBuilder.Append("&");
-			canonicalizedRequestBuilder.Append(Uri.EscapeDataString(parameterString));
+            if (optionalParams != null)
+            {
+                foreach (var optionalParam in optionalParams)
+                {
+                    authorizationParts.Remove(optionalParam.Key);
+                }
+            }
 
-			string signature = ComputeSignature(appSecret, accessTokenSecret, canonicalizedRequestBuilder.ToString());
-			authorizationParts.Add("oauth_signature", signature);
-			authorizationParts.Remove("oauth_verifier");
+            var authorizationHeaderBuilder = new StringBuilder();
+            authorizationHeaderBuilder.Append("OAuth ");
+            foreach (var authorizationPart in authorizationParts)
+            {
+                authorizationHeaderBuilder.AppendFormat(
+                    "{0}=\"{1}\", ", authorizationPart.Key, Uri.EscapeDataString(authorizationPart.Value));
+            }
+            authorizationHeaderBuilder.Length = authorizationHeaderBuilder.Length - 2;
+            var authorizationHeader = authorizationHeaderBuilder.ToString();
+            return authorizationHeader;
+        }
 
-			if (optionalParams != null)
-			{
-				foreach (var optionalParam in optionalParams)
-				{
-					authorizationParts.Remove(optionalParam.Key);
-				}
-			}
+        public static SortedDictionary<string, string> GetAuthorizationParts(string appId, string accessToken, string nonce, string oauthTimestamp, Dictionary<string, string> optionalParams)
+        {
+            var authorizationParts = new SortedDictionary<string, string>()
+            {
+                {"oauth_consumer_key", appId},
+                {"oauth_nonce", nonce},
+                {"oauth_signature_method", "HMAC-SHA1"},
+                {"oauth_token", accessToken},
+                {"oauth_timestamp", oauthTimestamp},
+                {"oauth_version", "1.0"},
+            };
 
-			var authorizationHeaderBuilder = new StringBuilder();
-			authorizationHeaderBuilder.Append("OAuth ");
-			foreach (var authorizationPart in authorizationParts)
-			{
-				authorizationHeaderBuilder.AppendFormat(
-					"{0}=\"{1}\", ", authorizationPart.Key, Uri.EscapeDataString(authorizationPart.Value));
-			}
-			authorizationHeaderBuilder.Length = authorizationHeaderBuilder.Length - 2;
-			var authorizationHeader = authorizationHeaderBuilder.ToString();
-			return authorizationHeader;
-		}
-	}
+            if (optionalParams != null)
+            {
+                foreach (var param in optionalParams)
+                {
+                    authorizationParts.Add(param.Key, param.Value);
+                }
+            }
+
+            return authorizationParts;
+        }
+    }
 }
