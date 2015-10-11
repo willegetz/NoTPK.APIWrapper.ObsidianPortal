fad9878093c1c77c5b2b182154a3da98bb426a63
William Getz <willegetz@gmail.com>
2015-10-11 12:57:06 -0700

    Refactored tests

--- NoTPK.APIWrapper.ObsidianPortal/APIWrapper.ObsidianPortal.Tests/RequestHelper_Tests.cs
+++ NoTPK.APIWrapper.ObsidianPortal/APIWrapper.ObsidianPortal.Tests/RequestHelper_Tests.cs
@@ -35,12 +35,7 @@ namespace APIWrapper.ObsidianPortal.Tests
         [TestMethod]
         public void TestGetAuthorizationParts()
         {
-            var optionalParams = new Dictionary<string, string>()
-            {
-                {"optionalKey1", "optionalValue1" },
-                {"optionalKey2", "optionalValue2" },
-                {"optionalKey3", "optionalValue3" },
-            };
+            Dictionary<string, string> optionalParams = GetOptinalParts();
 
             var result = RequestHelpers.GetAuthorizationParts(AppId, AccessToken, Nonce, OauthTimeStamp, optionalParams);
             Approvals.VerifyAll(result);
@@ -56,15 +51,7 @@ namespace APIWrapper.ObsidianPortal.Tests
         [TestMethod]
         public void TestBuildAuthorizationParameterString()
         {
-            var authorizationParts = new SortedDictionary<string, string>()
-            {
-                {"oauth_consumer_key", AppId},
-                {"oauth_nonce", Nonce},
-                {"oauth_signature_method", "HMAC-SHA1"},
-                {"oauth_token", AccessToken},
-                {"oauth_timestamp", OauthTimeStamp},
-                {"oauth_version", "1.0"}
-            };
+            var authorizationParts = GetAuthParts();
 
             var result = RequestHelpers.BuildAuthorizationParameterString(authorizationParts);
             Approvals.Verify(result);
@@ -95,33 +82,21 @@ namespace APIWrapper.ObsidianPortal.Tests
         [TestMethod]
         public void TestUpdateAuthorizationParts()
         {
-            var authorizationParts = new SortedDictionary<string, string>()
-            {
-                {"oauth_consumer_key", AppId},
-                {"oauth_nonce", Nonce},
-                {"oauth_signature_method", "HMAC-SHA1"},
-                {"oauth_token", AccessToken},
-                {"oauth_timestamp", OauthTimeStamp},
-                {"oauth_version", "1.0"}
-            };
-
-            var optionalParams = new Dictionary<string, string>()
-            {
-                {"optionalKey1", "optionalValue1" },
-                {"optionalKey2", "optionalValue2" },
-                {"optionalKey3", "optionalValue3" },
-            };
-
-            var signature = @"Lxbp/sGTkhBpCFjtlTL7P218iug=";
-
-            var result = RequestHelpers.UpdateAuthorizationParts(authorizationParts, optionalParams, signature);
+            var result = GetUpdatedAuthParts();
             Approvals.VerifyAll(result);
         }
 
         [TestMethod]
         public void TestBuildAuthorizationHeader()
         {
-            var authorizationParts = new SortedDictionary<string, string>()
+            var updatedAuthParts = GetUpdatedAuthParts();
+            var result = RequestHelpers.BuildAuthorizationHeader(updatedAuthParts);
+            Approvals.Verify(result);
+        }
+
+        private static SortedDictionary<string, string> GetAuthParts()
+        {
+            return new SortedDictionary<string, string>()
             {
                 {"oauth_consumer_key", AppId},
                 {"oauth_nonce", Nonce},
@@ -130,20 +105,26 @@ namespace APIWrapper.ObsidianPortal.Tests
                 {"oauth_timestamp", OauthTimeStamp},
                 {"oauth_version", "1.0"}
             };
+        }
 
-            var optionalParams = new Dictionary<string, string>()
+        private static Dictionary<string, string> GetOptinalParts()
+        {
+            return new Dictionary<string, string>()
             {
                 {"optionalKey1", "optionalValue1" },
                 {"optionalKey2", "optionalValue2" },
                 {"optionalKey3", "optionalValue3" },
             };
+        }
+
+        private static SortedDictionary<string, string> GetUpdatedAuthParts()
+        {
+            var authorizationParts = GetAuthParts();
+
+            var optionalParams = GetOptinalParts();
 
             var signature = @"Lxbp/sGTkhBpCFjtlTL7P218iug=";
-
-            var updatedAuthParts = RequestHelpers.UpdateAuthorizationParts(authorizationParts, optionalParams, signature);
-
-            var result = RequestHelpers.BuildAuthorizationHeader(updatedAuthParts);
-            Approvals.Verify(result);
+            return RequestHelpers.UpdateAuthorizationParts(authorizationParts, optionalParams, signature);
         }
     }
 }
